f99536a21c0be9b08d50e700025039f95e7107a6
JosseMoyano <josse.moyano@gmail.com>
2021-12-24 13:44:48 -0300

    finish style

--- src/views/Detalle.jsx
+++ src/views/Detalle.jsx
@@ -1,8 +1,128 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import '../styles/detalle.css'
+import goCloud from '../assets/icons/gc-icon_logo.svg'
+import logOut from '../assets/icons/gc-icon_logout.svg'
+import arrow from '../assets/icons/gc-icon_down-arrow.svg'
+import dialog from '../assets/icons/gc-icon_dialog.svg'
+import check from '../assets/icons/gc-icon_check.svg'
+import waiting from '../assets/icons/gc-icon_waiting.svg'
+import phone from '../assets/icons/gc-icon_phone.svg'
+import close from '../assets/icons/gc-icon_close.svg'
+import time from '../assets/icons/gc-icon_time.svg'
 
 export default function Detalle () {
 
+    let history = useHistory();
+
     return (
-        <div>Detalle</div>
+        <div className="detalle_container">
+            <div className="detalle_logos">
+                <div className="detalle_logo">
+                    <img src={goCloud} alt='LogoGoCloud' className="detalle_img_gocloud" /> 
+                </div>
+                <div className="detalle_logout" onClick={() => history.push("/login")}>
+                    <img src={logOut} alt='LogOut' className='detalle_img_logout' />   
+                </div>
+            </div>
+            <div className="detalle_omnia">
+                <p className="detalle_p_omnia">OMNI MIA</p>
+            </div>
+            <div className="detalle_container2">
+                <p className="detalle_titulo">Detalle de campaña</p>
+                <div className="detalle_container3">
+                    <div>
+                        <p>Nombre de la campaña</p>
+                    </div>
+                    <div className="detalle_subcontainer">
+                        <p><span style={{fontWeight:'600'}}>Progreso discado:</span> 73%</p>
+                        <p><span style={{fontWeight:'600'}}>Consumo total:</span> 02:07 min</p>
+                        <p><span style={{fontWeight:'600'}}>Duración promedio llamada:</span> 24seg</p>
+                    </div>
+                </div>
+                <div className="detalle_containercards">
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero1">36</p>
+                        <p className="detalle_p">transferidas</p>
+                        <img src={arrow} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line1"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero2">3728</p>
+                        <p className="detalle_p">Iniciadas</p>
+                        <img src={dialog} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line2"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero3">11712</p>
+                        <p className="detalle_p">Discadas</p>
+                        <img src={check} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line3"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero4">7984</p>
+                        <p className="detalle_p">No iniciadas</p>
+                        <img src={waiting} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line4"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero5">11712</p>
+                        <p className="detalle_p">Numeros</p>
+                        <img src={phone} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line5"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero6">10</p>
+                        <p className="detalle_p">Fallidas</p>
+                        <img src={close} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line6"></div>
+                    </div>
+                    <div className="detalle_card">
+                        <p className="detalle_p_numero7">14</p>
+                        <p className="detalle_p">Compromiso Pago</p>
+                        <img src={time} alt='arrow' className="detalle_img" />
+                        <div className="detalle_line7"></div>
+                    </div>
+                </div>
+                    <table className="detalle_table" >
+                            <tr className="detalle_tr">
+                                <th className="detalle_thead th1">TELÉFONO</th>
+                                <th className="detalle_thead">FECHA</th>
+                                <th className="detalle_thead">HORA</th>
+                                <th className="detalle_thead">DURACIÓN</th>
+                                <th className="detalle_thead">ULT. ETAPA</th>
+                                <th className="detalle_thead">ULT. PASO</th>
+                                <th className="detalle_thead th7">INTENTOS</th>
+                            </tr>
+                        <tr>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                        </tr>
+                        <tr>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                        </tr>
+                        <tr>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                            <td className="detalle_td">bla bla bla</td>
+                        </tr>
+                    </table>
+            </div>
+        </div>
     )
 }
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import '../styles/login.css'
 import goCloud from '../assets/gc-logo-default.svg'
 import robot from "../assets/gc-robot.svg"
 
 export default function Login () {
+
+    let history = useHistory();
 
     return (
         <div className="login_container">
@@ -25,7 +29,7 @@ export default function Login () {
                     </label>
                 </div>
                 <div>
-                    <button className="login_button">Ingresar</button>
+                    <button className="login_button" onClick={() => history.push("/detalle")}>Ingresar</button>
                 </div>
             </div>
         </div>
